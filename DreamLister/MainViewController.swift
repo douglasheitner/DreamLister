72cd8c956e740fac81dbd4142d84a81b72403186
Douglas Heitner <douglas.heitner@gmail.com>
2017-02-20 11:40:45 -0600

    Initial Commit

---
+++ DreamLister/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  DreamLister
+//
+//  Created by Douglas Heitner on 2/17/17.
+//  Copyright © 2017 Douglas Heitner. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
