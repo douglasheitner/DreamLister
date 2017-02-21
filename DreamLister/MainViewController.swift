c95c0680372916d5303314d2ebbd529732696add
Douglas Heitner <douglas.heitner@gmail.com>
2017-02-21 15:04:06 -0800

    Finished front end design

--- DreamLister/MainViewController.swift
+++ DreamLister/MainViewController.swift
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     
 
 }
