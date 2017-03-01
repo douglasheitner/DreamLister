f3648fe8618194d0667cd4e735a71c7d51f6ca9e
Douglas Heitner <douglas.heitner@gmail.com>
2017-03-01 15:00:15 -0800

    Finished the Add/Edit UI.

---
+++ DreamLister/ItemsDetailsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ItemsDetailsViewController.swift
+//  DreamLister
+//
+//  Created by Douglas Heitner on 2/28/17.
+//  Copyright © 2017 Douglas Heitner. All rights reserved.
+//
+
+import UIKit
+
+class ItemsDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
