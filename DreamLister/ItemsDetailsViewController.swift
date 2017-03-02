ba4734fd17d408c247837523c095ab932af897ba
Douglas Heitner <douglas.heitner@gmail.com>
2017-03-02 14:59:06 -0800

    Minor changes to the ItemDetailsViewController.

--- DreamLister/ItemsDetailsViewController.swift
+++ DreamLister/ItemsDetailsViewController.swift
@@ -8,32 +8,32 @@
 
 import UIKit
 
-class ItemsDetailsViewController: UIViewController {
+class ItemsDetailsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
 
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        
+        storePicker.dataSource = self
+        storePicker.delegate = self
+        
         }
         
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 0
     }
-    */
-
 }
